c3f2a7297b4bf6e5a4245d6049cd31d79a7c2fff
atmosplayz <135615624+atmosplayz@users.noreply.github.com>
2023-06-05 18:15:17 +0530

    Add files via upload

---
+++ secert.py
@@ -0,0 +1,142 @@
+#alp========a b c d e f g h i j k l m n o p q r s t u v w x y z
+#ALP========Z Y X W V U T S R Q P O N M L K J I H G F E D C B A
+
+while True:
+    AS = raw_input("encoder or decoder")
+
+    if AS == "encoder" or "Encoder":
+        print("if your pc or laptop is slow you may have to repeat the sentence or number again on this output script")
+        AX = raw_input("")
+        BX = ""
+
+
+    if AS == "decoder" or "Decoder":
+        print("if your pc or laptop is slow you may have to repeat the sentence or number again on this output script")
+        OX = raw_input("")
+        PX = ""
+
+
+    for char in AX:
+        if char == "a" or char == "A":
+            BX += "1"
+        if char == "b" or char == "B":
+            BX += "2"
+        if char == "c" or char == "C":
+            BX += "3"
+        if char == "d" or char == "D":
+            BX += "4"
+        if char == "e" or char == "E":
+            BX += "5"
+        if char == "f" or char == "F":
+            BX += "6"
+        if char == "g" or char == "G":
+            BX += "7"
+        if char == "h" or char == "H":
+            BX += "8"
+        if char == "i" or char == "I":
+            BX += "9"
+        if char == "j" or char == "J":
+            BX += "0"
+        if char == "k" or char == "K":
+            BX += "!"
+        if char == "l" or char == "L":
+            BX += "@"
+        if char == "m" or char == "M":
+            BX += "#"
+        if char == "n" or char == "N":
+            BX += "$"
+        if char == "o" or char == "O":
+            BX += "%"
+        if char == "p" or char == "P":
+            BX += "^"
+        if char == "q" or char == "Q":
+            BX += "&"
+        if char == "r" or char == "R":
+            BX += "*"
+        if char == "s" or char == "S":
+            BX += "("
+        if char == "t" or char == "T":
+            BX += ")"
+        if char == "u" or char == "U":
+            BX += "-"
+        if char == "v" or char == "V":
+            BX += "+"
+        if char == "w" or char == "W":
+            BX += "["
+        if char == "x" or char == "X":
+            BX += "]"
+        if char == "y" or char == "Y":
+            BX += ";"
+        if char == "z" or char == "Z":
+            BX += "'"
+        if char == " ":
+            BX += "_"
+    print("")
+    print(BX)
+
+    for num in OX:
+        if num == "1":
+            PX += "a"
+        if num == "2":
+            PX += "b"
+        if num == "3":
+            PX += "c"
+        if num == "4":
+            PX += "d"
+        if num == "5":
+            PX += "e"
+        if num == "6":
+            PX += "f"
+        if num == "7":
+            PX += "g"
+        if num == "8":
+            PX += "h"
+        if num == "9":
+            PX += "i"
+        if num == "0":
+            PX += "j"
+        if num == "!":
+            PX += "k"
+        if num == "@":
+            PX += "l"
+        if num == "#":
+            PX += "m"
+        if num == "$":
+            PX += "n"
+        if num == "%":
+            PX += "o"
+        if num == "^":
+            PX += "p"
+        if num == "&":
+            PX += "q"
+        if num == "*":
+            PX += "r"
+        if num == "(":
+            PX += "s"
+        if num == ")":
+            PX += "t"
+        if num == "-":
+            PX += "u"
+        if num == "+":
+            PX += "v"
+        if num == "[":
+            PX += "w"
+        if num == "]":
+            PX += "x"
+        if num == ";":
+            PX += "y"
+        if num == "'":
+            PX += "z"
+        if num == "_":
+            PX += " "
+    print("")
+    print(PX)
+
+
+
+
+
+
+        
+
+
